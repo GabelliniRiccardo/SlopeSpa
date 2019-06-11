89e49d331c338c0294a900d4f38d79d7a37ad185
Riccardo <gabelliniriccardo.94@gmail.com>
2019-06-11 18:14:42 +0200

    Added fixtures for User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordEncoderInterface $password_encoder)
+    {
+        $this->password_encoder = $password_encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$name, $last_name, $email, $password, $roles])
+        {
+            $user = new User();
+            $user->setName($name);
+            $user->setLastName($last_name);
+            $user->setEmail($email);
+            $user->setPassword($this->password_encoder->encodePassword($user, $password));
+            $user->setRoles($roles);
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [
+
+            ['Riccardo', 'Gabellini', 'gabelliniriccardo.94@gmail.com', 'riccard0', ['ROLE_ADMIN']],
+
+        ];
+    }
+}
--- src/Migrations/Version20190611161256.php
+++ src/Migrations/Version20190611161256.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190611160209 extends AbstractMigration
+final class Version20190611161256 extends AbstractMigration
 {
     public function getDescription() : string
     {
