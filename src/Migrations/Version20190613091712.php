2bd48d23c9d55136de93a842d3fc1bd1e3fc89a6
Riccardo <gabelliniriccardo.94@gmail.com>
2019-06-13 09:18:05 +0000

    removed argon21 as a password encoder

--- src/Migrations/Version20190613091712.php
+++ src/Migrations/Version20190613091712.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190613080514 extends AbstractMigration
+final class Version20190613091712 extends AbstractMigration
 {
     public function getDescription() : string
     {
